5b88b68a6718c972005e4cbfce583ea8ec8f5461
Gaurav Singla <97586538+itzgauravsingla@users.noreply.github.com>
2022-03-04 12:28:06 +0530

    Add files via upload

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+ 
+
+  constructor(private loggingService:LoggingService, private accountsService:AccountsService) {}
+
+  onSetTo(status: string) {
+    this.accountsService.updateAccount(this.id, status);
+    // console.log('A server status changed, new status: ' + status);
+    // this.loggingService.loggingService(status);
+    this.accountsService.stausUpdated.emit(status);
+  }
+}
