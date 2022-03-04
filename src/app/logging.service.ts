30c96fe9b168aa6bc5059f96d893fac2544a231a
Gaurav Singla <97586538+itzgauravsingla@users.noreply.github.com>
2022-03-04 12:26:52 +0530

    Delete src/app directory

--- src/app/logging.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-
-export class LoggingService{
-    loggingService(status: string){
-        console.log("A server status changed, new status: " + status)
-    }
-}
